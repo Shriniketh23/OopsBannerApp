d2c51ab9cd4ae669a31a1e842536a28d4ea24b7f
Shriniketh23 <sb1137@srmist.edu.in>
2026-03-13 09:21:50 +0530

    feature/uc3

--- src/OopsBannerApp.java
+++ src/OopsBannerApp.java
@@ -1,9 +1,10 @@
 class OopsBannerApp{
     public static void main(String [] args){
-        System.out.println("  *  " + "     **   "+ "****   "+"   **");
-        System.out.println("*    *" + "  *    * "+ "*   *"+" ** ");
-        System.out.println("*    *" + "  *    * "+ "**** "+"  **");
-        System.out.println("*    *" + "  *    * "+ "*     "+"    **");
-        System.out.println("  **  " + "    **   "+ "*     "+" ** ");
+      System.out.println(String.join(" ", " *** ", " *** ", " **** ", " **** "));
+System.out.println(String.join(" ", "** **", "** **", "**  **", "**   "));
+System.out.println(String.join(" ", "** **", "** **", "**** ", " *** "));
+System.out.println(String.join(" ", "** **", "** **", "**   ", "   **"));
+System.out.println(String.join(" ", "** **", "** **", "**   ", "**  **"));
+System.out.println(String.join(" ", " *** ", " *** ", "**   ", " **** "));
     }
 }
