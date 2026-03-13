9de85758cc5d114625d68fd432d6132c7c2712ce
Shriniketh23 <sb1137@srmist.edu.in>
2026-03-13 10:58:58 +0530

    feature/uc6

--- src/OopsBannerApp.java
+++ src/OopsBannerApp.java
@@ -1,17 +1,52 @@
 class OopsBannerApp{
     public static void main(String [] args){
        String[] lines = {
-    String.join(" ", " *** ", " *** ", " **** ", " **** "),
-    String.join(" ", "** **", "** **", "**  **", "**   "),
-    String.join(" ", "** **", "** **", "**** ", " *** "),
-    String.join(" ", "** **", "** **", "**   ", "   **"),
-    String.join(" ", "** **", "** **", "**   ", "**  **"),
-    String.join(" ", " *** ", " *** ", "**   ", " **** ")
-};
+    public class BannerOPS {
 
-// Then use a for-each loop to print each line
-for (String line : lines) {
-    System.out.println(line);
+    // Pattern for O
+    public static String[] getOPattern() {
+        return new String[]{
+            " *** ",
+            "*   *",
+            "*   *",
+            "*   *",
+            " *** "
+        };
+    }
+
+    // Pattern for P
+    public static String[] getPPattern() {
+        return new String[]{
+            "**** ",
+            "*   *",
+            "**** ",
+            "*    ",
+            "*    "
+        };
+    }
+
+    // Pattern for S
+    public static String[] getSPattern() {
+        return new String[]{
+            " ****",
+            "*    ",
+            " *** ",
+            "    *",
+            "**** "
+        };
+    }
+
+    public static void main(String[] args) {
+
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(oPattern[i] + "   " + pPattern[i] + "   " + sPattern[i]);
+        }
+    }
 }
+
     }
 }
