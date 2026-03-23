ddcd820d172abe8e2c736a976d5edbcaa056f325
Shriniketh23 <sb1137@srmist.edu.in>
2026-03-23 10:32:44 +0530

    week1level1

--- src/problem1.java
+++ src/problem1.java
@@ -1,9 +1,22 @@
-public class problem1 {
+import java.util.Scanner;
+
+class OopsBannerApp{
     public static void main(String[] args) {
-         float avg=(94+95+96)/3;
-     System.out.print(avg);
+         System.out.print("Enter the number");
+         Scanner sc=new Scanner(System.in);
+         int n=sc.nextInt();
+         if(n%5==0){
+            System.out.println("number is divisible by 5");
+         }
+         else{
+            System.out.print("number not divisible by 5");
+         }
         
     }
+   
+    
+
+}
    
    
 
